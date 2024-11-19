8cb09da54390257ff00bf72797756bb28c6bbf80
ROBINSON MOLINA <robinson.granados01@gmail.com>
2024-11-19 16:49:08 -0500

    Solution error import and exports

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,51 +1,49 @@
-import { gql } from 'apollo-server';
-
-export const typeDefs = gql`
+const typeDefs = `
   type Department {
-    id: ID!
-    name: String!
-    numEmployees: Int!
+    id: Int
+    name: String
+    numEmployees: Int
     employees: [Employee]
   }
 
   type Employee {
-    id: ID!
-    name: String!
+    id: Int
+    name: String
     phone: String
     email: String
     salary: Float
     department: Department
   }
 
+  input DepartmentInput {
+    name: String
+    numEmployees: Int
+  }
+
+  input EmployeeInput {
+    name: String
+    phone: String
+    email: String
+    salary: Float
+    departmentId: Int
+  }
+
   type Query {
     departments: [Department]
-    department(id: ID!): Department
-    employees(departmentId: ID!): [Employee]
-    employee(id: ID!): Employee
+    department(id: Int): Department
+    employees(departmentId: Int): [Employee]
+    employee(id: Int): Employee
   }
 
   type Mutation {
-    createDepartment(name: String!, numEmployees: Int!): Department
-    updateDepartment(id: ID!, name: String!, numEmployees: Int!): Department
-    deleteDepartment(id: ID!): String
+    createDepartment(input: DepartmentInput!): Department
+    updateDepartment(id: Int, input: DepartmentInput!): Department
+    deleteDepartment(id: Int): Boolean
 
-    createEmployee(
-      name: String!,
-      phone: String,
-      email: String,
-      salary: Float,
-      departmentId: ID!
-    ): Employee
-
-    updateEmployee(
-      id: ID!,
-      name: String,
-      phone: String,
-      email: String,
-      salary: Float,
-      departmentId: ID
-    ): Employee
-
-    deleteEmployee(id: ID!): String
+    createEmployee(input: EmployeeInput!): Employee
+    updateEmployee(id: Int, input: EmployeeInput!): Employee
+    deleteEmployee(id: Int): Boolean
   }
 `;
+
+module.exports = typeDefs;
--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./graphql/schema');
-const resolvers = require('./graphql/resolvers');
-require('dotenv').config();
+const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const resolvers = require('./graphql/resolvers.js');
+const typeDefs = require('./graphql/schema.js');
 
-const server = new ApolloServer({
+const startServer = async () => {
+  // Crear el servidor Apollo
+  const apolloServer = new ApolloServer({
     typeDefs,
     resolvers,
-});
+  });
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-    console.log(`Server ready at ${url}`);
+  // Inicializar Apollo Server
+  await apolloServer.start();
+
+  // Crear la aplicación Express
+  const app = express();
+
+  // Middleware para Apollo Server
+  app.use(
+    '/graphql',
+    cors(),
+    bodyParser.json(),
+    expressMiddleware(apolloServer)
+  );
+
+  // Levantar el servidor en el puerto 3000
+  app.listen(3000, () => {
+    console.log('Server ready at http://localhost:3000/graphql');
+  });
+};
+
+startServer().catch((error) => {
+  console.error('Error starting the server:', error);
 });
